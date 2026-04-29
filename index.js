70cd48172e6efbabfabfba34971a856f75d30a8f
juanrg888 <54454259+Juanrg888@users.noreply.github.com>
2026-04-29 13:00:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/', async () => {
   return { status: 'API funcionando' }
 })
 
+app.get('/health', async () => {
+  return { status: 'ok' }
+})
+
 app.get('/db', async () => {
   const result = await pool.query('SELECT NOW() as tiempo')
   return { db: 'conectada', tiempo: result.rows[0].tiempo }
